1e22176f2d06341b90be5addcfd7b1be8e06785b
Melih Bahar <>
2024-01-30 18:15:12 +0200

    ci test

--- .infrastructure/provider.tf
+++ .infrastructure/provider.tf
@@ -1,9 +1,3 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-
 provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIA5FTY6OVRBLIY67LG"
-  secret_key = "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"
+  region = "us-east-1"
 }
